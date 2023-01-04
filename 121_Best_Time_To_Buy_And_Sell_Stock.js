c39fd9791c7dbbafb451113500b38179920b0acf
lexCarey <lexcarey73@gmail.com>
2023-01-03 17:32:23 -0800

    121 2nd Try, Works Better & Faster

--- 121_Best_Time_To_Buy_And_Sell_Stock.js
+++ 121_Best_Time_To_Buy_And_Sell_Stock.js
@@ -1,4 +1,29 @@
+//2nd try, works better and faster!!
 
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let highestProfit = 0
+    let runner = 0
+    let lowest = prices[runner]
+    let highest = prices[runner]
+    while (runner < prices.length) {
+        if (lowest > prices[runner]) {
+            lowest = prices[runner]
+            highest = 0
+        }
+        if (highest < prices[runner]) {
+            highest = prices[runner]
+        }
+        if (highest - lowest > highestProfit) {
+            highestProfit = highest - lowest
+        }
+        runner++
+    }
+    return highestProfit
+};
 
 
 //Works but too slow
