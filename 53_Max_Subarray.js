1be7cf90933b75a8a1dd27dabfd13090ba23c6e4
lexCarey <lexcarey73@gmail.com>
2023-03-14 14:20:41 -0700

    53 Fast

--- 53_Max_Subarray.js
+++ 53_Max_Subarray.js
@@ -1,23 +1,21 @@
-// Slow answer o(n^2)
+// Fast Answer Using Kadane's Method O(n)
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let largestSum = nums[0]
-    let currentSum
-    for (let i = 0; i<nums.length; i++) {
-        currentSum = nums[i]
-        if (currentSum>largestSum) {
-                largestSum = currentSum
-            }
-        for (let j = i+1; j<nums.length; j++) {
-            currentSum = currentSum + nums[j]
-            if (currentSum>largestSum) {
-                largestSum = currentSum
-            }
+    let currentMax = nums[0]
+    let overallMax = nums[0]
+    for (let i=1; i<nums.length; i++) {
+        if (nums[i] > currentMax + nums[i]) {
+            currentMax = nums[i]
+        } else {
+            currentMax = currentMax + nums[i]
+        }
+        if (currentMax > overallMax) {
+            overallMax = currentMax
         }
     }
-    return largestSum
+    return overallMax
 };
